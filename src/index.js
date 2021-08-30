959f2e6a9f69b60b5d0717bf8bfd76fbc2586195
‘gcd’ <it_gcd@163.com>
2021-08-31 06:57:56 +0800

    修复标题

--- src/index.js
+++ src/index.js
@@ -12,13 +12,13 @@ Vue.use(VueRouter)
 
 const routes = [
     {path:"",redirect:"/home"},
-    {path:"/home",component:home,
+    {path:"/home",meta:{title:'首页'},component:home,
     children:[{path:"",component:news},
               {path:"news",component:news},
               {path:"mes",component:mes}]},
-    {path:"/about",component:about},
-    {path:"/user/:id",component:user},
-    {path:"/profile",component:profile}
+    {path:"/about",meta:{title:'关于'},component:about},
+    {path:"/user/:id",meta:{title:'用户'},component:user},
+    {path:"/profile",meta:{title:'档案'},component:profile}
 ];
 
 const router = new VueRouter({
@@ -26,5 +26,9 @@ const router = new VueRouter({
     mode: 'history',
   })
 
+  router.beforeEach((to, from, next) => {
+    document.title = to.matched[0].meta.title
+    next()
+  })
 
 export default router
